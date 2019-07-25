5b31d8598c2ae75f1ff218e62968d9069434537f
chrstnsz <mytraxx@outlook.com>
2019-07-25 16:14:08 +0200

    update layout view

--- php/functions.php
+++ php/functions.php
@@ -2,10 +2,6 @@
 
 function logged_only(){
 
-	// if(session_status() == PHP_SESSION_NONE){
-	// session_start();
-	// }
-
 	if(!isset($_SESSION['auth'])){
 	$_SESSION['flash']['danger'] = "Vous n'avez pas le droit d'accéder a cette page";
 	header('Location: index.php');
--- php/logout.php
+++ php/logout.php
@@ -5,6 +5,7 @@ session_start();
 unset($_SESSION['auth']);
 unset($_SESSION['perso']);
 unset($_SESSION['backpack']);
+unset($_SESSION['LAST_ACTIVITY']);
 
 $_SESSION['flash']['success'] = "Vous êtes maintenant déconnecté(e).";
 
